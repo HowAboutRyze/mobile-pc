9ba0f5df2f743a738e2fb59cb6b41584054441ae
HowAboutRyze <lizhongwei157@163.com>
2019-07-08 16:45:11 +0800

    测试一下vue-particles

--- src/components/pc/HelloWorld.vue
+++ src/components/pc/HelloWorld.vue
@@ -3,6 +3,25 @@
     <h1>{{ msg }}</h1>
     <h2>这是pc</h2>
     <router-link to="/haha">哈哈</router-link>
+    <vue-particles
+        color="#dedede"
+        :particleOpacity="0.7"
+        :particlesNumber="80"
+        shapeType="circle"
+        :particleSize="4"
+        linesColor="#dedede"
+        :linesWidth="1"
+        :lineLinked="true"
+        :lineOpacity="0.4"
+        :linesDistance="150"
+        :moveSpeed="3"
+        :hoverEffect="true"
+        hoverMode="grab"
+        :clickEffect="true"
+        clickMode="push"
+        class="particles-js"
+      >
+      </vue-particles>
   </div>
 </template>
 
@@ -32,5 +51,20 @@ li {
 }
 a {
   color: #42b983;
+}
+</style>
+<style scoped>
+h1, h2, a {
+  color: #fff;
+}
+.particles-js {
+    background-image: url(../../assets/img/sky.jpg);
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    z-index: -1;
 }
 </style>
--- src/framework/pc/index.js
+++ src/framework/pc/index.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from '../../router/pc'
 
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
